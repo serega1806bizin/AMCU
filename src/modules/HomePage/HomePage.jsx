c042759f38e4027a0c936895e58ebbdb418eda56
serega1806bizin <seregabizin@icloud.com>
2025-04-21 19:00:53 +0300

    gf

--- src/modules/HomePage/HomePage.jsx
+++ src/modules/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import { S1 } from './Sections/s1';
 import { S2 } from './Sections/s2';
 import { S3 } from './Sections/s3';
+import { S4 } from './Sections/s4';
 
 export const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ export const HomePage = () => {
       <S1 />
       <S2 />
       <S3 />
+      <S4 />
     </>
   );
 };
---
+++ src/modules/HomePage/Sections/s4.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useAnimateInView } from '../useAnimateInView';
+
+const posts = [
+  {
+    title: 'Partnership Forum',
+    // eslint-disable-next-line max-len
+    text: 'Щоб краще познайомитися з нами, приходь на наші заходи. В календарі заходів анонси всіх активностей, які проводить наша організація.',
+  },
+  {
+    title: 'Partnership Forum',
+    // eslint-disable-next-line max-len
+    text: 'Щоб краще познайомитися з нами, приходь на наші заходи. В календарі заходів анонси всіх активностей, які проводить наша організація.',
+  },
+  {
+    title: 'Partnership Forum',
+    // eslint-disable-next-line max-len
+    text: 'Щоб краще познайомитися з нами, приходь на наші заходи. В календарі заходів анонси всіх активностей, які проводить наша організація.',
+  },
+  {
+    title: 'Partnership Forum',
+    // eslint-disable-next-line max-len
+    text: 'Щоб краще познайомитися з нами, приходь на наші заходи. В календарі заходів анонси всіх активностей, які проводить наша організація.',
+  },
+];
+
+const PostCard = ({ post, delay }) => {
+  const ref = useAnimateInView();
+
+  return (
+    <div
+      ref={ref}
+      className="post-card can-animate"
+      style={{ animationDelay: delay }}
+    >
+      <h3>{post.title}</h3>
+      <p>{post.text}</p>
+    </div>
+  );
+};
+
+export const S4 = () => {
+  const sectionTitleRef = useAnimateInView();
+
+  return (
+    <div id="s4" className="screen">
+      <div className="container_res">
+        <div ref={sectionTitleRef} className="col">
+          <h2>Результати нашої діяльності</h2>
+        </div>
+        <div className="posts-wrapper">
+          <div className="posts-grid">
+            {posts.map((post, index) => (
+              <PostCard key={index} post={post} delay={`${index * 0.2}s`} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
